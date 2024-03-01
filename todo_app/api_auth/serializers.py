1eaa9fbd082635f06609661d2684b9cf4d70549e
Meri Ismail <142440680+Meriitoo@users.noreply.github.com>
2024-03-01 21:08:19 +0200

    Initial commit

---
+++ todo_app/api_auth/serializers.py
@@ -0,0 +1,42 @@
+from django.contrib.auth import password_validation, get_user_model
+from django.core import exceptions
+from rest_framework import serializers
+
+UserModel = get_user_model()
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = ('username', 'password')
+
+    # This hashes the password (Not save in plain-text in the DB)
+    def create(self, validated_data):
+        user = super().create(validated_data)
+
+        user.set_password(user.password)
+        user.save()
+
+        return user
+
+    # Invoke password validators
+    def validate(self, data):
+        user = UserModel(**data)
+        password = data.get('password')
+        errors = {}
+
+        try:
+            password_validation.validate_password(password, user)
+        except exceptions.ValidationError as e:
+            errors['password'] = list(e.messages)
+
+        if errors:
+            raise serializers.ValidationError(errors)
+
+        return super().validate(data)
+
+    # In response in browser console, we just see the name we send, without pass
+    def to_representation(self, instance):
+        user_representation = super().to_representation(instance)
+        user_representation.pop('password')
+        return user_representation
